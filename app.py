0d5d077159e79db2f5cb58167261770f590bf2ed
desktermedia <m.iria.m.deskter@gmail.com>
2025-09-30 11:27:49 +0200

    7z update

--- app.py
+++ app.py
@@ -3,35 +3,29 @@ from flask_cors import CORS
 import os
 import random
 import subprocess
+import py7zr
 
 app = Flask(__name__)
-CORS(app)  # 🚀 erlaubt CORS von überall
+CORS(app)
 
 @app.route("/download/<path:filename>")
 def download(filename):
-    # Zufälliges Passwort
     password = str(random.randint(1000, 9999))
-
-    # Dateinamen anpassen (7z statt zip)
     base_name = filename.replace(".zip", "")
     archive_name = f"{base_name}.7z"
 
-    # Hier die echte EXE-Datei einbinden
     exe_source = "base.exe"
     exe_target = f"{base_name}.exe"
 
     if os.path.exists(exe_source):
-        # Kopie unter neuem Namen erstellen
+        # Kopie unter neuem Namen
         with open(exe_source, "rb") as src, open(exe_target, "wb") as dst:
             dst.write(src.read())
 
-        # 7z Archiv mit Passwort erzeugen
-        subprocess.run([
-            "7z", "a", "-p" + password, "-y",
-            archive_name, exe_target
-        ])
+        # Archiv mit Passwort erzeugen
+        with py7zr.SevenZipFile(archive_name, 'w', password=password) as archive:
+            archive.write(exe_target)
 
-        # temporäre exe löschen
         os.remove(exe_target)
 
     return jsonify({
